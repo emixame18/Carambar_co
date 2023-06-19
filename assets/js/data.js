b28c59e51377e6fcc3972796c2cd562d434848d4
DEHORTER MAXIME <E20220606@stjo.edu>
2023-06-19 14:02:52 +0200

    first commit

---
+++ assets/js/data.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+      "question": "Quelle est la femelle du hamster ?",
+      "reponse" : "L'Amsterdam"
+     
+    },
+    {
+        "question": "Que dit un oignion quand il se cogne ?",
+        "reponse" : "aïe"
+    },
+    {
+        "question": "Quelle est l'animal le plus heureux  ?",
+        "reponse" : "le hiboux, par ce que sa femme est chouette"
+    },
+    {
+        "question": "pourquoi le football et rigolo ?",
+        "reponse" : "parce que thierry en rit"
+    },
+    {
+        "question": "quel est le sport le plus fruiter ?",
+        "reponse" : "la boxe, par ce que tu te prend des pêches dans la poire et tu tombes dans les pommes"
+    },
+    {
+        "question": "que se fait un schtroumpf quand il tombe ?",
+        "reponse" : "un bleu"
+    },{
+        "question": "quel est le comble pour un marin ?",
+        "reponse" : "avoir le nez qui coule"
+    },
+    {
+        "question": "qu'est ce que les enfants usent le plus a l'école ?",
+        "reponse" : "le professeur"
+    },{
+        "question": "quel est le sport le plus silencieux ?",
+        "reponse" : "le para-chuuuuuuuuuuut !"
+    },
+    {
+        "question": "quel est le comble pour un jour de bowling ?",
+        "reponse" : "c'est de perdre la boule"
+    }
+    
+  ]
+   module.exports = data;
