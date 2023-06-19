424357c3db2bc54c244da654432a977c2ec5a8f9
emixame18 <118394696+emixame18@users.noreply.github.com>
2023-06-19 15:18:50 +0200

    Add files via upload

--- assets/js/data.js
+++ assets/js/data.js
@@ -50,5 +50,24 @@ const Blagues = [
       reponse: "C'est de perdre la boule"
     }
   ];
-  
+  const jokeButton = document.getElementById("jokeButton");
+const jokeContainer = document.getElementById("jokeContainer");
+
+jokeButton.addEventListener("click", function() {
+    const randomJoke = Blagues[Math.floor(Math.random() * Blagues.length)];
+
+    // Création des éléments HTML pour la question et la réponse
+    const questionElement = document.createElement("p");
+    questionElement.textContent = randomJoke.question;
+
+    const reponseElement = document.createElement("p");
+    reponseElement.textContent = randomJoke.reponse;
+
+    // Effacement du contenu précédent du conteneur
+    jokeContainer.innerHTML = "";
+
+    // Ajout des éléments de la blague au conteneur
+    jokeContainer.appendChild(questionElement);
+    jokeContainer.appendChild(reponseElement);
+});
   module.exports = Blagues;
